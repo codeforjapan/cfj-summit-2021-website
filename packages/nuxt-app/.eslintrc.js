a5cfbd650e87dac05ec8f0b566a41475afcbad6b
MaySoMusician <maysomusician@gmail.com>
2021-02-10 18:44:09 +0900

    Update ESLint configurations

--- packages/nuxt-app/.eslintrc.js
+++ packages/nuxt-app/.eslintrc.js
@@ -1,10 +1,13 @@
 module.exports = {
+  plugins: ['prettier'],
   extends: [
     '../../.eslintrc.json',
     '@nuxtjs/eslint-config-typescript',
     'plugin:nuxt/recommended',
     'prettier',
     'prettier/@typescript-eslint',
+    'prettier/vue',
+    'plugin:prettier/recommended',
   ],
   ignorePatterns: ['!**/*', '.eslintrc.js'],
   env: {
